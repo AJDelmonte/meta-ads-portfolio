b78d6aa9131b33dfd29b32fabdde5e2914559723
AJDelmonte <aleliedelmonte0824@gmail.com>
2026-07-29 16:18:57 +0800

    Complete Day 4 final portfolio

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Portfolio Loaded Successfully 🚀");
+const backToTop = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.style.display = 'flex';
+  } else {
+    backToTop.style.display = 'none';
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
